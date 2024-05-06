02627223ffb5ef04d22023afb60b89d48ba74a0b
gabriel <gabriel.trabalon@gmail.com>
2024-05-06 11:47:52 -0300

    ToDo List

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import { StatusBar } from 'react-native';
+import {
+  useFonts, 
+  Inter_400Regular,
+  Inter_700Bold
+} from '@expo-google-fonts/inter'
+import { Home } from './src/pages';
+import { Loading } from './src/components/Loading';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_700Bold
+  });
+  return (
+    <>
+      {fontsLoaded ? <Home />: <Loading />}
+      <StatusBar 
+      barStyle='light-content' 
+      backgroundColor="transparent"
+      translucent
+      />
+    </>
+  )
+}
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,40 @@
+import { View, Image, TextInput, TouchableOpacity} from "react-native";
+import { styles } from "./styles";
+import logo from '../../assets/logo.png';
+import { theme } from "../../../theme";
+import {MaterialCommunityIcons} from '@expo/vector-icons'
+
+type HeaderProps = {
+    task: string,
+    inputRef: React.RefObject<TextInput>,
+    onChangeText: (task: string) => void,
+    onPress: () => void,
+    children?: React.ReactNode
+};
+
+export function Header({ task, inputRef, onChangeText, onPress }: HeaderProps) {
+    return<View style={styles.headerContainer}>
+        <Image source={logo}/>
+        <View style={styles.form}>
+            <TextInput 
+            style={[
+                styles.input, 
+                inputRef.current?.isFocused() && task
+                 ? styles.inputBorder 
+                 : null
+                ]} 
+            placeholder="Adicione uma nova tarefa"
+            placeholderTextColor={theme.colors.base.gray300}
+            value={task}
+            onChangeText={onChangeText}
+            ref={inputRef}
+            autoCorrect={false}
+            onSubmitEditing={onPress}
+            returnKeyType="done"
+            />
+            <TouchableOpacity style={styles.button} onPress={onPress}>
+                <MaterialCommunityIcons name="plus-circle-outline" size={22} color={theme.colors.base.gray100}/>
+            </TouchableOpacity>
+        </View>
+    </View>
+}
